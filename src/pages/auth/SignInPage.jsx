3dd610fc88e0376218e231caff03212b83608296
Matt Marashi <matt@marashi.dev>
2024-08-07 00:38:07 -0400

    initial setup

---
+++ src/pages/auth/SignInPage.jsx
@@ -0,0 +1,9 @@
+const SignInPage = () => {
+  return (
+    <div className="font-lato text-blue-600">
+      Sign In <i className="fa-solid fa-lock"></i>
+    </div>
+  );
+};
+
+export default SignInPage;
---
+++ src/pages/auth/SignUpPage.jsx
@@ -0,0 +1,5 @@
+const SignUpPage = () => {
+  return <div className="font-playfair text-green-500">Sign Up</div>;
+};
+
+export default SignUpPage;
